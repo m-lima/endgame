bdfe69f7008a05f918e14bd5ce06809bfefe0657
Marcelo Lima <m-lima@users.noreply.github.com>
2025-10-22 18:16:05 +0200

    Preparing nginx compilation

---
+++ README.md
@@ -0,0 +1,8 @@
+# EndGame
+
+## Options
+
+* Whitelist source auth bypass (e.g. 127.0.0.1)
+* OpenIDC configuration
+* API access (set a header to activate the flow. The header is not CORS allowed)
+* User allow list per location
--- flake.nix
+++ flake.nix
@@ -20,9 +20,88 @@
 
   outputs =
     {
+      nixpkgs,
+      fenix,
       flake-utils,
       helper,
       ...
     }@inputs:
-    flake-utils.lib.eachDefaultSystem (system: (helper.lib.rust.helper inputs system ./. { }).outputs);
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system} // {
+          overlays = [ fenix.overlays.default ];
+        };
+        nginx = pkgs.nginx.override {
+          modules = [
+            pkgs.nginxModules.moreheaders
+            {
+              name = "endgame";
+              meta = {
+                description = "OpenIDC handler for nginx";
+                license = [ pkgs.lib.licenses.mit ];
+              };
+              src = ./.;
+            }
+          ];
+          # buildPhase = ""; # "runHook preBuild && runHook postBuild";
+          # installPhase = ''
+          #   runHook preInstall
+          #   cp -a $src $out
+          #   runHook postInstall
+          # '';
+        };
+        # nginx = pkgs.stdenv.mkDerivation {
+        #   name = "${pkgs.nginx.name}-src";
+        #
+        #   src = pkgs.nginx.src;
+        #
+        #   nativeBuildInputs = [
+        #     pkgs.pcre
+        #   ];
+        #
+        #   configurePhase = "runHook preConfigure && runHook postConfigure";
+        #   buildPhase = "runHook preBuild && runHook postBuild";
+        #   installPhase = ''
+        #     runHook preInstall
+        #     mkdir -p $out
+        #     tar -xzf $src -C $out --strip-components=1
+        #     $out/configure --without-http_gzip_module
+        #     runHook postInstall
+        #   '';
+        # };
+        outputs =
+          (helper.lib.rust.helper inputs system ./. {
+            binary = false;
+            # nativeBuildInputs = pkgs: [ pkgs.nginx.src ];
+            overrides = {
+              commonArgs = {
+                NGINX_SOURCE_DIR = nginx;
+              };
+
+              # devShell = {
+              #   NGINX_SOURCE_DIR = nginx;
+              # };
+
+              # commonArgs =
+              #   default:
+              #   (builtins.removeAttrs default [ "src" ])
+              #   // {
+              #     srcs = [
+              #       default.src
+              #       pkgs.nginx.src
+              #     ];
+              #
+              #     unpackPhase = "ls -lah && false";
+              #   };
+            };
+          }).outputs;
+      in
+      outputs
+      // {
+        packages = outputs.packages // {
+          inherit nginx;
+        };
+      }
+    );
 }
