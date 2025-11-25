85fa85606c55839697b1d5b9efeaf7543b97f89b
Marcelo Lima <m-lima@users.noreply.github.com>
2025-11-25 16:41:28 +0100

    Use new again

--- main.c
+++ main.c
@@ -15,9 +15,10 @@ int test_header(Key key) {
   CSlice in_given = endgame_c_slice_new(NULL, 0);
   CSlice in_family = endgame_c_slice("  family	");
 
-  printf("In Email:   %.*s\n", uintptr_t_to_int(in_email.len), in_email.ptr);
-  printf("In Given:   %.*s\n", uintptr_t_to_int(in_given.len), in_given.ptr);
-  printf("In Family:  %.*s\n", uintptr_t_to_int(in_family.len), in_family.ptr);
+  printf("In Email:   '%.*s'\n", uintptr_t_to_int(in_email.len), in_email.ptr);
+  printf("In Given:   '%.*s'\n", uintptr_t_to_int(in_given.len), in_given.ptr);
+  printf("In Family:  '%.*s'\n", uintptr_t_to_int(in_family.len),
+         in_family.ptr);
 
   RustSlice encrypted = endgame_rust_slice_null();
   Error error =
@@ -36,12 +37,14 @@ int test_header(Key key) {
             out_family = endgame_rust_slice_null();
   error = endgame_decrypt(&key, param, 30, &out_email, &out_given, &out_family);
   if (error.ptr != NULL) {
-    printf("Error: %.*s\n", uintptr_t_to_int(error.len), error.ptr);
+    printf("Error: '%.*s'\n", uintptr_t_to_int(error.len), error.ptr);
     return 1;
   }
-  printf("Out Email:  %.*s\n", uintptr_t_to_int(out_email.len), out_email.ptr);
-  printf("Out Given:  %.*s\n", uintptr_t_to_int(out_given.len), out_given.ptr);
-  printf("Out Family: %.*s\n", uintptr_t_to_int(out_family.len),
+  printf("Out Email:  '%.*s'\n", uintptr_t_to_int(out_email.len),
+         out_email.ptr);
+  printf("Out Given:  '%.*s'\n", uintptr_t_to_int(out_given.len),
+         out_given.ptr);
+  printf("Out Family: '%.*s'\n", uintptr_t_to_int(out_family.len),
          out_family.ptr);
 
   endgame_rust_slice_free(&encrypted);
